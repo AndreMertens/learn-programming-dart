605c3ca91ab52525d734665f0c60712ae7d2a25e
Andre Mertens <andre.mertens@inform-software.com>
2025-01-23 12:51:13 +0100

    change task

--- 23_task/23_task.dart
+++ 23_task/23_task.dart
@@ -1,7 +1,12 @@
 // Qudrierte Quersumme
 
 // Aufgabe:
-// Schreibe eine Funktion die berechnet, ob n eine Primzahl ist.
+// Schreibe eine Funktion die die quadrierte Quersumme berechnet.
+
+// Beispiel:
+// 5 => 5^2 = 25
+// 47 => (4 + 7)^2 = 11^2 = 121
+// 121 => (1 + 2 + 1)^2 = 4^2 = 16
 
 int getSquaredCrossSum(int n) {
   return 0;
